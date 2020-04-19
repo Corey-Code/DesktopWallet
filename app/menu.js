e34b854c42ebcf61ad21b311d34eb3407116f5da
Corey-Code <37006206+Corey-Code@users.noreply.github.com>
2020-04-19 15:34:30 -0400

    Upgrade Dennis, add menu controls

--- app/menu.js
+++ app/menu.js
@@ -1,76 +1,86 @@
 // @flow
-import path from 'path';
-import { openExternal } from './utils/open-external';
-import { openItem } from './utils/open-item';
-import packageJson from '../package.json';
-import { getVidulumFolder } from '../config/daemon/get-vidulum-folder';
-import { isTestnet } from '../config/is-testnet';
+import path from "path";
+import { openExternal } from "./utils/open-external";
+import { openItem } from "./utils/open-item";
+import packageJson from "../package.json";
+import { getVidulumFolder } from "../config/daemon/get-vidulum-folder";
+import { isTestnet } from "../config/is-testnet";
 
-const isMac = process.platform === 'darwin';
-const DOCS_URL = 'https://vidulum.app/';
-const VDL_CONF_PATH = path.join(getVidulumFolder(), 'vidulum.conf');
-const VDL_MASTERNODE_CONF_PATH = path.join(getVidulumFolder(), 'masternode.conf');
+const isMac = process.platform === "darwin";
+const DISCORD_URL = "https://discord.gg/QhV2Wx6";
+const VDL_CONF_PATH = path.join(getVidulumFolder(), "vidulum.conf");
+const VDL_MASTERNODE_CONF_PATH = path.join(
+  getVidulumFolder(),
+  "masternode.conf"
+);
 const EXPLORER_URL = isTestnet()
-  ? 'https://vdlt-explorer.vidulum.app/'
-  : 'https://explorer.vidulum.app/';
-const WEBWALLET_URL = 'https://wallet.vidulum.app';
+  ? "https://vdlt-explorer.vidulum.app/"
+  : "https://explorer.vidulum.app/";
+const WEBWALLET_URL = "https://wallet.vidulum.app";
+const BOOTSTRAP_URL = "https://downloads.vidulum.app";
 
 const submenu = [
   {
-    label: 'Open vidulum.conf',
+    label: "Open Web Wallet",
+    click() {
+      openExternal(WEBWALLET_URL);
+    },
+  },
+  {
+    label: "Download Bootstrap",
+    click() {
+      openExternal(BOOTSTRAP_URL);
+    },
+  },
+  {
+    label: "Open vidulum.conf",
     click() {
       openItem(VDL_CONF_PATH);
     },
   },
   {
-    label: 'Open masternode.conf',
+    label: "Open masternode.conf",
     click() {
       openItem(VDL_MASTERNODE_CONF_PATH);
     },
   },
   {
-    label: 'Open Explorer',
+    label: "Open VDL Explorer",
     click() {
       openExternal(EXPLORER_URL);
     },
   },
+  { type: "separator" },
   {
-    label: 'Open Webwallet',
-    click() {
-      openExternal(WEBWALLET_URL);
-    },
-  },
-  { type: 'separator' },
-  {
-    label: 'Exit',
-    role: isMac ? 'close' : 'quit',
+    label: "Exit",
+    role: isMac ? "close" : "quit",
   },
 ];
 
 const menu = [
   {
-    label: 'Menu',
+    label: "Menu",
     submenu,
   },
 ];
 
 const helpMenu = {
-  role: 'help',
+  role: "help",
   submenu: [
     {
-      label: `Vidulum Desktop Wallet Version v${packageJson.version}`,
+      label: `Desktop Wallet v${packageJson.version}`,
       enabled: false,
     },
     {
-      label: 'Get Help',
+      label: "Get Help on Discord",
       click() {
-        openExternal(DOCS_URL);
+        openExternal(DISCORD_URL);
       },
     },
   ],
 };
 
-if (process.platform === 'darwin') {
+if (process.platform === "darwin") {
   menu.unshift({
     label: packageJson.name,
     submenu,
@@ -78,9 +88,7 @@ if (process.platform === 'darwin') {
 
   menu.push({
     ...helpMenu,
-    submenu: [
-      ...helpMenu.submenu,
-    ],
+    submenu: [...helpMenu.submenu],
   });
 } else {
   menu.push(helpMenu);
